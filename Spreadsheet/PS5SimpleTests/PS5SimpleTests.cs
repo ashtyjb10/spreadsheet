49e5870ad1c7af7f9c5d2381fe45dfa5e5c8c91e
Nathan Herrmann <Newtex001@hotmail.com>
2018-02-11 02:32:14 -0700

    Start Setup

---
+++ Spreadsheet/PS5SimpleTests/PS5SimpleTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SS;
+
+namespace PS5SimpleTests
+{
+    [TestClass]
+    public class PS5SimpleTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            AbstractSpreadsheet sheet = new Spreadsheet();
+        }
+    }
+}
---
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -0,0 +1,47 @@
+﻿//Created by Nathan Herrmann for CS 3500
+using System;
+using System.Collections.Generic;
+using Formulas;
+
+namespace SS
+{
+    public class Spreadsheet : AbstractSpreadsheet
+    {
+
+        public Spreadsheet()
+        {
+
+        }
+
+
+        public override object GetCellContents(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override IEnumerable<string> GetNamesOfAllNonemptyCells()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, double number)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, string text)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override ISet<string> SetCellContents(string name, Formula formula)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override IEnumerable<string> GetDirectDependents(string name)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
