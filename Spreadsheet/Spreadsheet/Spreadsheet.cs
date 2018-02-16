ecbca779f8bb7c0fabf5b4a2d2a13a11359c0825
Nathan Herrmann <Newtex001@hotmail.com>
2018-02-15 23:39:13 -0700

    Final Commit, Grade this.

--- Spreadsheet/Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet/Spreadsheet.cs
@@ -2,19 +2,24 @@
 using System;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
+using Dependencies;
 using Formulas;
 
 namespace SS
 {
     public class Spreadsheet : AbstractSpreadsheet
     {
+        const string namePattern = @"([a-zA-Z+])([1-9])(\d+)?";
+        private Dictionary<string, Cell> nonEmptyCells;
+        private DependencyGraph dependencyGraph;
 
         /// <summary>
         /// Creates and empty spreadsheet
         /// </summary>
         public Spreadsheet()
         {
-
+            nonEmptyCells = new Dictionary<string, Cell>();
+            dependencyGraph = new DependencyGraph();
         }
 
         /// <summary>
@@ -25,18 +30,24 @@ namespace SS
         /// </summary>
         public override object GetCellContents(string name)
         {
-            string namePattern = @"([a-zA-Z+])([1-9])(\d+)?";
-
             if (name == null || !Regex.IsMatch(name, namePattern))
             {
                 throw new InvalidNameException();
             }
+            
+            //Try to find the cell that has the content.
+            this.nonEmptyCells.TryGetValue(name, out Cell returnedCell);
 
-
-
-
-
-            return null;
+            //If the content in the cell is null, then it is an empty cell and an empty string is returned.
+            if (returnedCell.GetContent() == null)
+            {
+                return "";
+            }
+            //Otherwise return the content.
+            else
+            {
+                return returnedCell.GetContent();
+            }        
         }
 
         /// <summary>
@@ -44,7 +55,16 @@ namespace SS
         /// </summary>
         public override IEnumerable<string> GetNamesOfAllNonemptyCells()
         {
-            throw new NotImplementedException();
+            //The set of all names to be returned.
+            HashSet<string> cellNames = new HashSet<string>();
+
+            //Populate the set.
+            foreach (Cell cell in this.nonEmptyCells.Values)
+            {
+                cellNames.Add(cell.GetName());
+            }
+
+            return cellNames;
         }
 
         /// <summary>
@@ -59,8 +79,36 @@ namespace SS
         /// </summary>
         public override ISet<string> SetCellContents(string name, double number)
         {
-            throw new NotImplementedException();
-        }
+            //Null name check and pattern matching
+            if (name == null || !Regex.IsMatch(name, namePattern))
+            {
+                throw new InvalidNameException();
+            }
+
+            //If the cell exists in the nonEmpty list then it must be removed and replacesd along
+            //with the dependencies
+            if (nonEmptyCells.ContainsKey(name))
+            {
+                dependencyGraph.ReplaceDependees(name, new HashSet<string>());
+                nonEmptyCells.Remove(name);
+            }
+            
+            //Create the new cell
+            Cell newCell = new Cell(name, number);
+            nonEmptyCells.Add(name, newCell);
+            
+            //Create the list to be returned
+            HashSet<string> toReturn = new HashSet<string>();
+            toReturn.Add(name);
+
+            //Populate the list
+            foreach (string dependent in dependencyGraph.GetDependents(name))
+            {
+                toReturn.Add(dependent);
+            }
+            
+            return toReturn;
+    }
 
         /// <summary>
         /// If text is null, throws an ArgumentNullException.
@@ -76,7 +124,49 @@ namespace SS
         /// </summary>
         public override ISet<string> SetCellContents(string name, string text)
         {
-            throw new NotImplementedException();
+            //Null check
+            if(text == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            //Null check and regex check
+            if (name == null || !Regex.IsMatch(name, namePattern))
+            {
+                throw new InvalidNameException();
+            }
+
+            //If the content is an empty string, the cell is empty and does not have any dependency other than itself.
+            if (text.Equals(""))
+            {
+                HashSet<string> toReturnEmpty = new HashSet<string>();
+                toReturnEmpty.Add(name);
+                return toReturnEmpty;
+            }
+
+            //If the nonempty cell contains the name, the the original cell and dependencies must
+            //be removed.
+            if (nonEmptyCells.ContainsKey(name))
+            {
+                dependencyGraph.ReplaceDependees(name, new HashSet<string>());
+                nonEmptyCells.Remove(name);
+            }
+
+            //If cell is not empty, make a new, non-empty cell
+            Cell newCell = new Cell(name, text);
+            nonEmptyCells.Add(name, newCell);
+
+            //Create the list to be returned
+            HashSet<string> toReturn = new HashSet<string>();
+            toReturn.Add(name);
+
+            //Populate list
+            foreach (string dependency in dependencyGraph.GetDependents(name))
+            {
+                toReturn.Add(dependency);
+            }
+
+            return toReturn;
         }
 
         /// <summary>
@@ -96,7 +186,46 @@ namespace SS
         /// </summary>
         public override ISet<string> SetCellContents(string name, Formula formula)
         {
-            throw new NotImplementedException();
+            //Null check.
+            if (formula.Equals(null))
+            {
+                throw new ArgumentNullException();
+            }
+
+            //Null and regex check on name.
+            if (name == null || !Regex.IsMatch(name, namePattern))
+            {
+                throw new InvalidNameException();
+            }
+
+            //If the cell exits, it along with all its dependencies must be removed
+            if (nonEmptyCells.ContainsKey(name))
+            {
+                dependencyGraph.ReplaceDependees(name, new HashSet<string>());
+                nonEmptyCells.Remove(name);
+            }
+
+            //Create a new cell and add it to the cell list.
+            Cell newCell = new Cell(name, formula);
+            nonEmptyCells.Add(name, newCell);
+
+            //HashSet to return to the caller created.
+            HashSet<string> toReturn = new HashSet<string>();
+            toReturn.Add(name);
+
+            //Set up dependencies.
+            foreach (string token in formula.GetVariables())
+            {
+                if(Regex.IsMatch(token, namePattern))
+                {
+                    dependencyGraph.AddDependency(token, name);
+                }
+            }
+
+            //Get all the direct and indirect dependencies.
+            GetAllDependents(name, name, toReturn);
+
+            return toReturn;
         }
 
         /// <summary>
@@ -118,7 +247,97 @@ namespace SS
         /// </summary>
         protected override IEnumerable<string> GetDirectDependents(string name)
         {
-            throw new NotImplementedException();
+            //Null check
+            if(name == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            //regex check
+            if (!Regex.IsMatch(name, namePattern))
+            {
+                throw new InvalidNameException();
+            }
+            
+            //create the list to be returned
+            List<string> directDependencies = new List<string>();
+
+            //Populate list with dependencies
+            foreach (string dependent in this.dependencyGraph.GetDependents(name))
+            {
+                directDependencies.Add(dependent);
+            }
+
+            return directDependencies;
+        }
+
+        /// <summary>
+        /// Helper method that will add all dependencies, direct and indirect, to an ISet<string>
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="set"></param>
+        private void GetAllDependents(string start, string name, ISet<string> set)
+        {
+            //Get all the dependencies of the named cell.
+            foreach (string dependency in dependencyGraph.GetDependees(name))
+            {
+                //If the top cell is dependenct on itself, throw an exception
+                if (dependency.Equals(start))
+                {
+                    throw new CircularException();
+                }
+
+                //If the set does not already have the depenedncy listed, add it
+                if(!set.Contains(dependency))
+                {
+                    set.Add(dependency);
+                    GetAllDependents(start, dependency, set);
+                }
+            }
+        }
+        
+        /// <summary>
+        /// Private struct that is used to represent a cell in a spreadhseet, contains the name of the cell
+        /// and the content of the cell as either a string, double or Formula.
+        /// </summary>
+        private struct Cell
+        {
+            private string name;
+            private object content;
+
+            //String constructor
+            public Cell(string name, string content)
+            {
+                this.name = name;
+                this.content = content;
+            }
+
+
+            //Double constructor
+            public Cell(string name, double content)
+            {
+                this.name = name;
+                this.content = content;
+            }
+
+
+            //Formula constructor
+            public Cell(string name, Formula content)
+            {
+                this.name = name;
+                this.content = content;
+            }
+
+            public object GetContent()
+            {
+                return this.content;
+            }
+
+            public string GetName()
+            {
+                return this.name;
+            }
+
         }
     }
 }
--- Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
+++ Spreadsheet/SpreadsheetTests/SpreadsheetTests.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using Formulas;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SS;
 
@@ -7,12 +9,262 @@ namespace SpreadsheetTests
     [TestClass]
     public class SpreadsheetTests
     {
+        /// <summary>
+        /// Determins if the name is acceptable
+        /// </summary>
         [TestMethod]
-        //[ExpectedException(typeof(InvalidNameException))]
-        public void ATestMethod()
+        public void nameTest1()
         {
             AbstractSpreadsheet sheet = new Spreadsheet();
             sheet.GetCellContents("AAcd31");
+        }
+
+        /// <summary>
+        /// Verifies  that the return content method works on an empty cell
+        /// </summary>
+        [TestMethod]
+        public void GetCellContent()
+        {
+            AbstractSpreadsheet sheet = new Spreadsheet();
+            sheet.SetCellContents("A1", 12);
+            string returned = sheet.GetCellContents("A2").ToString();
+
+            Assert.AreEqual(returned, "");
+        }
+
+        /// <summary>
+        /// Determines if the get cell content works on a nonempty cell
+        /// </summary>
+        [TestMethod]
+        public void GetCellContent2()
+        {
+            AbstractSpreadsheet sheet = new Spreadsheet();
+            sheet.SetCellContents("A1", 12);
+            string returned = sheet.GetCellContents("A1").ToString();
+
+            Assert.AreEqual(returned, "12");
+        }
+
+        /// <summary>
+        /// Ensures that an inccorect cell name is detected.
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(InvalidNameException))]
+        public void GetCellContent3()
+        {
+            AbstractSpreadsheet sheet = new Spreadsheet();
+            sheet.SetCellContents("A;1", 12);
+            
+        }
+        ///<summary>
+        /// A1 contains 3
+        /// B1 contains the formula A1 * A1
+        /// C1 contains the formula B1 + A1
+        /// D1 contains the formula B1 - C1
+        /// The direct dependents of A1 are B1 and C1
+        /// <summary>
+        [TestMethod]
+        public void GetCellContent4()
+        {
+            AbstractSpreadsheet sheet = new Spreadsheet();
+            Formula f = new Formula("A1 * A1");
+            Formula f2 = new Formula("B1 + A1");
+            Formula f3 = new Formula("B1 - C1");
+            int index = 0;
+
+            ISet<string> set;
+            HashSet<string> checkSet = new HashSet<string>();
+            checkSet.Add("A1");
+            checkSet.Add("B1");
+            checkSet.Add("C1");
+
+            sheet.SetCellContents("B1", f);
+            sheet.SetCellContents("C1", f2);
+            sheet.SetCellContents("D1", f3);
+            set = sheet.SetCellContents("A1", 3);
+
+            foreach (string dep in set)
+            {
+                Assert.AreEqual(true, checkSet.Contains(dep));
+            }
+
+        }
+        ///<summary>
+        ///Adds a cell and changes the content, no exception thrown
+        ///<summary>
+        [TestMethod]
+        public void DoubleAdd1()
+        {
+            Spreadsheet sheet = new Spreadsheet();
+            sheet.SetCellContents("A1", 5);
+            sheet.SetCellContents("A2", 2);
+            sheet.SetCellContents("A1", 3);
+
+        }
+        ///<summary>
+        ///Changes a double to a string.
+        ///<summary>
+        [TestMethod]
+        public void DoubleAdd2()
+        {
+            Spreadsheet sheet = new Spreadsheet();
+            sheet.SetCellContents("A1", 3);
+            sheet.SetCellContents("A2", 2);
+            sheet.SetCellContents("A1", "grow");
+
+        }
+        /// <summary>
+        /// Changes a double to a formula 
+        /// </summary>
+        [TestMethod]
+        public void DoubleAdd3()
+        {
+            Spreadsheet sheet = new Spreadsheet();
+            Formula f = new Formula("2");
+            sheet.SetCellContents("A1", 5);
+            sheet.SetCellContents("A2", 2);
+            sheet.SetCellContents("A1", f);
+
+        }
+        /// <summary>
+        /// Returns the names of a single nonempty cell
+        /// </summary>
+        [TestMethod]
+        public void GetNames1()
+        {
+            Spreadsheet sheet = new Spreadsheet();
+            sheet.SetCellContents("A1", 3);
+            string[] trueNames = new string[] { "A1" };
+
+
+            IEnumerable<string> names = sheet.GetNamesOfAllNonemptyCells();
+
+            foreach (string checkName in names)
+            {
+                if (!(trueNames[0].Equals(checkName)))
+                {
+                    Assert.AreEqual(true, false);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns the names of the nonempty cells, and does not treat "D1" as nonempty
+        /// </summary>
+        [TestMethod]
+        public void GetNames2()
+        {
+            Spreadsheet sheet = new Spreadsheet();
+            sheet.SetCellContents("A1", "forest");
+            sheet.SetCellContents("B1", "for");
+            sheet.SetCellContents("C1", "the");
+            sheet.SetCellContents("A1", "trees");
+            sheet.SetCellContents("D1", "");
+            string[] trueNames = new string[] { "A1", "B1", "C1" };
+            int index = 0;
+            IEnumerable<string> names = sheet.GetNamesOfAllNonemptyCells();
+
+            foreach (string checkName in names)
+            {
+                if (!(trueNames[index].Equals(checkName)))
+                {
+                    Assert.AreEqual(true, false);
+                }
+                index++;
+            }
+        }
+        ///<summary>
+        ///For example, if name is A1, B1 contains A1*2, and C1 contains B1+A1, the
+        /// set {A1, B1, C1} is returned.
+        ///<summary>
+        [TestMethod]
+        public void CheckIndirectDependencies1()
+        {
+            ISet<string> check;
+            string[] trueValue = new string[3];
+            trueValue[0] = "A1";
+            trueValue[1] = "B1";
+            trueValue[2] = "C1";
+            
+            Spreadsheet sheet = new Spreadsheet();
+            Formula f = new Formula("A1*2");
+            Formula f2 = new Formula("B1+A1");
+            
+            sheet.SetCellContents("B1", f);
+            sheet.SetCellContents("C1", f2);
+            check = sheet.SetCellContents("A1", 3);
+            foreach (string dependent in trueValue)
+            {
+                if (!check.Contains(dependent))
+                {
+                    Assert.AreEqual(false, true);
+                }
+            }
+        }
+
+        /// <summary>
+        /// WHen a formula is changed the depenedncies that existed are removed.
+        /// </summary>
+        [TestMethod]
+
+        public void ClearDependencies1()
+        {
+            ISet<string> check;
+            string[] trueValue = new string[3];
+            trueValue[0] = "A1";
+            trueValue[1] = "B1";
+            trueValue[2] = "C1";
+            
+            Spreadsheet sheet = new Spreadsheet();
+            Formula f = new Formula("A1 *2");
+            Formula f2 = new Formula("B1 + A1");
+            Formula f3 = new Formula("A4");
+            sheet.SetCellContents("A1", f3);
+            sheet.SetCellContents("B1", f);
+            sheet.SetCellContents("C1", f2);
+            check = sheet.SetCellContents("A1", 3);
+
+            foreach(string dependency in check)
+            {
+                if (!check.Contains(dependency))
+                {
+                    Assert.AreEqual(true, false);
+                }
+            }
+        }
+        /// <summary>
+        /// Checks for circular ecxetions
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(CircularException))]
+        public void CircularDependencies1()
+        {
+            
+            Spreadsheet sheet = new Spreadsheet();
+            Formula f1 = new Formula("1 + A2");
+            Formula f2 = new Formula("1 + A3");
+            Formula f3 = new Formula("1 + A1");
+            sheet.SetCellContents("A1", f1);
+            sheet.SetCellContents("A2", f2);
+            sheet.SetCellContents("A3", f3);
+            
+        }
+
+        /// <summary>
+        /// checks for circular ecxeptions indirect
+        /// </summary>
+        [TestMethod]
+        [ExpectedException(typeof(CircularException))]
+        public void CircularDependencies2()
+        {
+            Spreadsheet sheet = new Spreadsheet();
+            Formula f1 = new Formula("1 + A2");
+            Formula f2 = new Formula("1 + A3");
+            Formula f3 = new Formula("A3 + A3");
+            sheet.SetCellContents("A1", f1);
+            sheet.SetCellContents("A2", f2);
+            sheet.SetCellContents("A3", f3);
+
         }
     }
 }
