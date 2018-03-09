4b5d71fcf032d4f032b40fa3607b015c4a5f7012
ashtyjb10 <ashtonj7college@gmail.com>
2018-03-08 18:27:51 -0700

    added the controler to the Enter button.

--- Spreadsheet/SpreadsheetGUI/SSWindow.cs
+++ Spreadsheet/SpreadsheetGUI/SSWindow.cs
@@ -116,7 +116,8 @@ namespace SpreadsheetGUI
                 if (sender is TextBox)
                 {
                     TextBox txb = (TextBox)sender;
-                    
+                    Console.WriteLine(txb.Text);
+                    ContentsChanged(txb.Text);
                     MessageBox.Show(txb.Text);
                 }
             }
