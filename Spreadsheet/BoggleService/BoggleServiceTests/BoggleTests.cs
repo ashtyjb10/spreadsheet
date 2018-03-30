19c5776845c45acd42e219f3b6c3e56f6c3f63d4
Nathan Herrmann <Newtex001@hotmail.com>
2018-03-29 20:50:01 -0600

    File path hacked

--- Spreadsheet/BoggleService/BoggleServiceTests/BoggleTests.cs
+++ Spreadsheet/BoggleService/BoggleServiceTests/BoggleTests.cs
@@ -66,7 +66,8 @@ namespace Boggle
             IISAgent.Start(@"/site:""BoggleService"" /apppool:""Clr4IntegratedAppPool"" /config:""..\..\..\.vs\config\applicationhost.config""");
 
             string path = AppDomain.CurrentDomain.BaseDirectory;
-            StreamReader reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "/dictionary.txt");
+            path = path.Remove(path.Length - 10, 10);
+            StreamReader reader = new StreamReader(path + "/dictionary.txt");
             while (reader.Peek() > -1)
             {
                 string wordToAdd = reader.ReadLine();
